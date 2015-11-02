60673a73803e77131a4a405eab8dcb3abd132fc8
james woods <james.woods396@gmail.com>
2015-11-02 09:07:02 -0500

    update starter kit app.jsx template with firebase

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,35 @@ var React = require('react');
 var ReactFire = require('reactfire');
 var FireBase = require('firebase');
 var Header = require('./header');
+var rootUrl = 'add your firebase url from your account';
 
 
 var App = React.createClass({
 	mixins: [ ReactFire ],
 	getInitialState: function() {
 		return {
-			items: {}
+			items: {},
+      loaded: false
 		}
 	},
+  componentWillMount: function() {
+    fb = new Firebase(rootUrl + 'items/');
+    this.bindAsObject(fb, 'items');
+    fb.on('value', this.handleDataLoaded);
+  },
 	render: function() {
   	return <div>
       <h1>My Personal Starter Template</h1>
       <Header itemsStore={this.firebaseRefs.items} />
+      <div className={"content " + (this.state.loaded ? "loaded" : "")}>
+            <h2>"Update with component to be loaded"</h2>
+          </div>
+        </div>
   	</div>
-	}
+	},
+    handleDataLoaded: function() {
+      this.setState({loaded: true});
+    }
 });
 
 var element = React.createElement(App, {});
